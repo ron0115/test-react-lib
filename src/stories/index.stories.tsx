dc46e502262105f5e122572e87e3927da785ef1f
liangzhirong <liangzhirong@yy.com>
2020-07-16 11:47:16 +0800

    chore: update npm script

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { text, select, boolean } from '@storybook/addon-knobs'
 
-import { Button } from './'
+import { Button } from '..'
 
 const knobs = {
   variant: {
